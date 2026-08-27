8cfc392e15b6dc9d7acf5f7de61d9f39bcbac364
xieqiansong <xieqiansong@qq.com>
2026-08-27 14:30:03 +0800

    refactor(jdk8-common): 清理 App 示例代码

--- jdk8-platform/jdk8-common/src/main/java/lan/chaos/App.java
+++ jdk8-platform/jdk8-common/src/main/java/lan/chaos/App.java
@@ -1,33 +1,12 @@
 package lan.chaos;
 
-import cn.hutool.core.codec.Base62;
-import cn.hutool.core.codec.Base62Codec;
-
-import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * Hello world!
  *
  */
 abstract public class App {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("1", "1");
-
-        ConcurrentHashMap<String, Object> concurrentHashMap = new ConcurrentHashMap<>();
-        concurrentHashMap.put("1", "1");
-        concurrentHashMap.get("1");
-
-        ByteBuffer buffer = ByteBuffer.allocate(4);
-
-        int x = 5;
-        Object obj = (x>4) ? 99.9:"a";
-
-        System.out.println(Math.abs(Integer.MIN_VALUE));
-        System.out.println(Math.abs(Long.MIN_VALUE));
-
+        helloWorld();
     }
 
     public static void helloWorld() {
